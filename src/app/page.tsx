a688d0b61ffa8c44ff8168d5f4765d2a292ef840
Meta3252 <meta3252@gmail.com>
2026-03-18 11:43:15 +0700

    fix-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSlider from './components/HeroSlider';
 import AboutCompany from './components/AboutCompany';
 import Services from './components/Services';
 import LogoScroller from "./components/LogoScroller";
-import ContactForm from './components/contact/ContactForm';
 
 export default function Home() {
   return (
